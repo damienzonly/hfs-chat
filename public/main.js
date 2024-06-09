0f8d326517f8bc20cbd8a51e50b0cb986f634937
Massimo Melina <a@rejetto.com>
2024-06-09 17:41:20 +0200

    use-strict (for Safari compatibility)

--- public/main.js
+++ public/main.js
@@ -1,70 +1,72 @@
-const { h } = HFS;
-const { useState, useEffect, Fragment: frag } = HFS.React;
+"use strict";{
+    const { h } = HFS;
+    const { useState, useEffect, Fragment: frag } = HFS.React;
 
-HFS.onEvent('afterList', () => h(ChatApp));
+    HFS.onEvent('afterList', () => h(ChatApp));
 
-const props = {
-    form(m, sm) {
-        return {
-            async onSubmit(e) {
-                e.preventDefault();
-                await fetch('/~/api/chat/add', {
-                    'Content-Type': 'application/json',
-                    method: 'POST',
-                    body: JSON.stringify({ m })
-                });
-                sm('');
-            }
-        };
-    },
-    input(m, sm) {
-        return {
-            value: m,
-            onChange(e) {
-                sm(e.target.value);
-            },
-        };
-    },
-};
+    const props = {
+        form(m, sm) {
+            return {
+                async onSubmit(e) {
+                    e.preventDefault();
+                    await fetch('/~/api/chat/add', {
+                        'Content-Type': 'application/json',
+                        method: 'POST',
+                        body: JSON.stringify({ m })
+                    });
+                    sm('');
+                }
+            };
+        },
+        input(m, sm) {
+            return {
+                value: m,
+                onChange(e) {
+                    sm(e.target.value);
+                },
+            };
+        },
+    };
 
-function ChatMessage({ message }) {
-    const { u, m, ts } = message;
-    return h('div', { className: 'msg' },
-        h('div', { className: 'msg-ts' }, new Date(ts).toLocaleString()),
-        `${u || '[anon]'} - ${m}`
-    );
-}
-
-function ChatContainer({ messages: ms }) {
-    const [m, sm] = useState('');
-    const [collapsed, sc] = useState(true);
-    const mlist = ms.map((message, i) => h(ChatMessage, { key: i, message }));
-
-    const chatMessages = h('div', { className: 'chat-container' },
-        h('div', { className: 'chat-header' }, 'Chat',
-            HFS.iconBtn(collapsed ? '▲' : '▼', () => sc(x => !x), { title: HFS.t("collapse/expand") })),
-        !collapsed && h('div', { className: 'chat-messages' }, mlist),
-        !collapsed && h('form', props.form(m, sm), h('input', props.input(m, sm)))
-    );
-    return h(frag, null, chatMessages);
-}
-
-function ChatApp() {
-    const [msgs, sm] = useState([]);
-    async function load() {
-        sm(await fetch('/~/api/chat/list').then(v => v.json()).then(v => HFS._.map(v, (o,ts) => Object.assign(o, {ts}))));
+    function ChatMessage({ message }) {
+        const { u, m, ts } = message;
+        return h('div', { className: 'msg' },
+            h('div', { className: 'msg-ts' }, new Date(ts).toLocaleString()),
+            `${u || '[anon]'} - ${m}`
+        );
     }
-    useEffect(() => {
-        const eventSource = HFS.getNotifications('chat', (e, data) => {
-            if (e !== 'newMessage') return;
-            sm(old => [...old, data]);
-        });
-        load();
-        return () => {
-            eventSource.then(v => v.close());
-        };
-    }, []);
-    return h(ChatContainer, { messages: msgs });
+
+    function ChatContainer({ messages: ms }) {
+        const [m, sm] = useState('');
+        const [collapsed, sc] = useState(true);
+        const mlist = ms.map((message, i) => h(ChatMessage, { key: i, message }));
+
+        const chatMessages = h('div', { className: 'chat-container' },
+            h('div', { className: 'chat-header' }, 'Chat',
+                HFS.iconBtn(collapsed ? '▲' : '▼', () => sc(x => !x), { title: HFS.t("collapse/expand") })),
+            !collapsed && h('div', { className: 'chat-messages' }, mlist),
+            !collapsed && h('form', props.form(m, sm), h('input', props.input(m, sm)))
+        );
+        return h(frag, null, chatMessages);
+    }
+
+    function ChatApp() {
+        const [msgs, sm] = useState([]);
+        async function load() {
+            sm(await fetch('/~/api/chat/list').then(v => v.json()).then(v => HFS._.map(v, (o,ts) => Object.assign(o, {ts}))));
+        }
+        useEffect(() => {
+            const eventSource = HFS.getNotifications('chat', (e, data) => {
+                if (e !== 'newMessage') return;
+                sm(old => [...old, data]);
+            });
+            load();
+            return () => {
+                eventSource.then(v => v.close());
+            };
+        }, []);
+        return h(ChatContainer, { messages: msgs });
+    }
 }
 
 /**
